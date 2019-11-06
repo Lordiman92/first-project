f98e7b4567baccf03469db2ac306d079ca02142b
Lordiman92 <magyarbalazsadam@gmail.com>
2019-11-06 19:24:24 +0100

    elsőőőőő commitom evör

---
+++ test/szamol_test.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://lordiman92.github.io/first-project/")
+driver.find_element(By.XPATH, "/html/body/form/input[1]").send_keys("32")
+driver.find_element(By.XPATH, "/html/body/form/input[2]").send_keys("23")
+driver.find_element(By.XPATH, "/html/body/form/input[3]").click()
+
+h1 = driver.find_element(By.XPATH, "//h1").text
+print(h1)
+
+assert h1 == "Számológép"
+
+driver.implicitly_wait(20)
+
+driver.close()
+
+
+
+
