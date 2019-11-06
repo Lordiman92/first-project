73d56f901eb5b140bb329856c9b7ae15fa2ce37c
Lordiman92 <magyarbalazsadam@gmail.com>
2019-11-06 20:54:13 +0100

    elsőőőőő commitom evör

--- test/szamol_test.py
+++ test/szamol_test.py
@@ -3,7 +3,10 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://lordiman92.github.io/first-project/")
-driver.find_element(By.XPATH, "/html/body/form/div/input[1]").send_keys("32")
+#driver.find_element(By.XPATH, "/html/body/form/div/input[1]").send_keys("32")
+first_input_field = driver.find_element(By.XPATH, "/html/body/form/div/input[1]")
+first_input_field.send_keys("32")
+first_input_field.screenshot("first_input.png")
 driver.find_element(By.XPATH, "/html/body/form/div/input[2]").send_keys("23")
 driver.find_element(By.XPATH, "/html/body/form/div/input[3]").click()
 
@@ -12,8 +15,10 @@ eredmeny = driver.find_element(By.CSS_SELECTOR, "#result-input").get_attribute("
 print(h1)
 print(eredmeny)
 
+driver.save_screenshot("result.png")
+
 assert h1 == "Számológép"
-assert eredmeny == '53'
+assert eredmeny == '55'
 
 driver.close()
 
